87106dbb0de4f5f7261edc1bb86c6dbd94ea46c0
Anantha Kumaran <ananthakumaran@gmail.com>
2021-01-22 00:00:11 +0530

    2015 9

---
+++ 2015/9a.pl
@@ -0,0 +1,46 @@
+%-*- mode:prolog -*-
+
+:- use_module(library(dcg/basics)).
+:- use_module(library(yall)).
+:- use_module(library(apply)).
+
+line([]) -->
+    eos.
+
+line([X | Xs]) -->
+    string(F), { atom_codes(From, F) },
+    " to ",
+    string(T), { atom_codes(To, T) },
+    " = ",
+    integer(Distance), { X = route(From, To, Distance) },
+    "\n", !,
+    line(Xs).
+
+shortest(Places, Total) :-
+    member(From, Places),
+    delete(Places, From, Remaining),
+    shortest(Remaining, From, 0, Total).
+
+shortest([], _, X, X).
+shortest(Remaining, From, Current, Total) :-
+    member(To, Remaining),
+    route(From, To, Distance),
+    delete(Remaining, To, Remaining1),
+    Next is Current + Distance,
+    shortest(Remaining1, To, Next, Total).
+
+add_route(route(F, T, D), Sin, [F,T|Sin]) :-
+    assertz(route(F, T, D)),
+    assertz(route(T, F, D)).
+
+answer(X) :-
+    phrase_from_file(line(Routes), '9a.input'),
+    retractall(route(_, _, _)),
+    foldl(add_route, Routes, [], Duplicates),
+    list_to_ord_set(Duplicates, Places),
+    findall(D, shortest(Places, D), Ds, []),
+    min_member(X, Ds).
+
+%?- answer(X).
+%@ X = 251 ;
+%@ false.
